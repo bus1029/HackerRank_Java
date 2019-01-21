fbe19a3c0cef113b6de3929b20a325d4ade86695
bus1029 <bus1029@gmail.com>
2019-01-21 16:23:04 +0900

    Study: Currency Formatter
    해당 나라의 통화 단위를 사용하고 싶다면,
    NumberFormat.getCurrencyInstance(Locale.US).format(payment);
    로 사용할 수 있다.
    여기서 Locale에 없는 나라라면 Custom Locale을 생성해줘야 하는데,
    https://docs.oracle.com/javase/1.5.0/docs/guide/intl/locale.doc.html
    사이트에서 India의 Locale ID를 찾아볼 수 있다.
    Locale indiaLocale = new Locale("en", "IN");

---
+++ Practice/Day_13_Currency_Formatter/Solution.java
@@ -0,0 +1,38 @@
+package Practice.Day_13_Currency_Formatter;
+
+import java.util.*;
+import java.text.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+
+        // Make a New Locale (Using Language English and Country is INDIA)
+        /*
+        %%
+        Type: region
+        Subtag: IN
+        Description: India
+        Added: 2005-10-16
+        %%
+         */
+        Locale indiaLocale = new Locale("en", "IN");
+        String india = NumberFormat.getCurrencyInstance(indiaLocale).format(payment);
+
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
+
+
