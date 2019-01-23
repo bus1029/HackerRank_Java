0c443707af193817302a1dfcc3a5251b388d5ee4
bus1029 <bus1029@gmail.com>
2019-01-23 12:02:08 +0900

    Study: IP Address Regex
    정규표현식으로 IP Address를 잡아내려면 다음과 같은 사항을 고려한다.
    
    1. A.B.C.D의 형태가 되어야 한다.
    2. A, B, C, D 각각 0부터 255사이의 숫자가 올 수 있다.
    
    그래서, 위 2개의 사항을 고려하기 위해서는
    String zeroTo255 = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
    String pattern = zeroTo255 + "\\." + zeroTo255 + "\\." +
     zeroTo255 + "\\." + zeroTo255;
    
    모든 케이스들을 다 고려해서 정규표현식을 작성해야 한다.

---
+++ Practice/Strings/Day_21_Regex/Solution.java
@@ -0,0 +1,38 @@
+package Practice.Strings.Day_21_Regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+//Write your code here
+class MyRegex {
+    /*
+    (): 소괄호 안의 문자를 하나의 문자로 인식한다.
+    []: 문자의 집합이나 범위를 나타내며, 두 문자 사이는 -기호로 표현한다.
+    [] 내에서 ^가 선행하여 존재하면 not을 나타낸다.
+
+    {}: 횟수 또는 범위를 나타낸다.
+    \: 정규표현식 \는 확장문자. \다음에 일반 문자가 오면 특수문자로 취급하고,
+    \다음에 특수문자가 오면 그 문자 자체를 의미한다.
+
+    \\d{1,2}: 1~2자리의 숫자들을 잡아낸다.
+    (0|1)\\d{2}: 0이나 1로 시작하는 3자리 숫자들을 잡아낸다.
+    2[0-4]\\d: 200-249사이의 숫자들을 잡아낸다.
+    25[0-5]: 250-255 사이의 숫자들을 잡아낸다.
+     */
+    private String matches = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+    public String pattern = matches + "\\." + matches + "\\." + matches + "\\." + matches;
+}
+
